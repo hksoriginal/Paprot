cc2b9d72241bad88bd88c18869b29ca80d822b6b
Harshit Kumar <44989568+hksoriginal@users.noreply.github.com>
2024-05-25 21:38:18 +0530

    more refactoring

--- Presenters/get_rag_output_presenter.py
+++ Presenters/get_rag_output_presenter.py
@@ -24,47 +24,60 @@ console = Console()
 
 class GetRAGOutputPresenter(GetRAGOutputPresenterInterface):
 
+    def __init__(
+            self,
+            firewall: SecurityFirewall,
+            text_processing_interactor: TextProcessingInteractor,
+            huggingface_embeddings_interactor: HuggingfaceEmbeddingsInteractor,
+            retriever_interactor: RetrievalQAInteractor,
+            chroma_vector_store_interactor: VectorStoresInteractor,
+            load_local_llm_interactor: LoadLLMInteractor,
+            prompt_template_interactor: PromptTemplateInteractor,
+            validate_text_interactor: ValidateResultsInteractor):
+
+        self.firewall = firewall
+        self.text_processing_interactor = text_processing_interactor
+        self.huggingface_embeddings_interactor = huggingface_embeddings_interactor
+        self.retriever_interactor = retriever_interactor
+        self.chroma_vector_store_interactor = chroma_vector_store_interactor
+        self.load_local_llm_interactor = load_local_llm_interactor
+        self.prompt_template_interactor = prompt_template_interactor
+        self.validate_text_interactor = validate_text_interactor
+
     def get_output(self, input_query: str) -> Optional[Tuple[str, List[str]]]:
-
-        firewall = SecurityFirewall()
-        huggingface_embeddings_interactor = HuggingfaceEmbeddingsInteractor()
-        retriever_interactor = RetrievalQAInteractor()
-        chroma_vector_store_interactor = VectorStoresInteractor()
-        load_local_llm_interactor = LoadLLMInteractor()
-        prompt_template_interactor = PromptTemplateInteractor()
-        validate_text_interactor = ValidateResultsInteractor()
-        text_preprocessing_interactor = TextProcessingInteractor()
-
         profanity_exists = \
-            text_preprocessing_interactor.profanity_check(text=input_query)
+            self.text_processing_interactor.profanity_check(text=input_query)
         if profanity_exists:
             return PROFANITY_RESPONSE
 
         print("\t Checking Malicious code\t")
-        if firewall.check_malicious_code_input(prompt_text=input_query):
+        if self.firewall.check_malicious_code_input(prompt_text=input_query):
             return NO_CODE
         print("\t Checking Malicious prompt\t")
-        if firewall.check_malicious_string(prompt_text=input_query):
+        if self.firewall.check_malicious_string(prompt_text=input_query):
             return POLITE_REFUSAL
 
         print("\t Performing text processing\t")
         input_query = \
-            text_preprocessing_interactor.perform_text_processing(
+            self.text_processing_interactor.perform_text_processing(
                 text=input_query)
 
         print("\t Loading LLM\t")
-        local_llm = load_local_llm_interactor.load_llm_model(
+        local_llm = self.load_local_llm_interactor.load_llm_model(
             local_llm_path=os.path.abspath(
                 './../LocalLLM/zephyr-7b-beta.Q5_K_S.gguf'))
 
-        embeddings = huggingface_embeddings_interactor.get_embeddings_object()
+        embeddings = (self.huggingface_embeddings_interactor
+                      .get_embeddings_object())
         print("\t Getting Embedding Object\t")
 
         print("\t Loading Chroma DB\t")
-        chroma_vector_store = chroma_vector_store_interactor.load_vector_stores(
-            embedding_function=embeddings,
-            file_path=os.path.abspath('./../Persist_Directory/document_cosine')
-        )
+        chroma_vector_store = (
+            self.chroma_vector_store_interactor.load_vector_stores(
+                embedding_function=embeddings,
+                file_path=os.path.abspath(
+                    './../Persist_Directory/document_cosine')
+            ))
 
         retriever = chroma_vector_store.as_retriever(
             search_kwargs=TOP_RESULTS,
@@ -72,7 +85,7 @@ class GetRAGOutputPresenter(GetRAGOutputPresenterInterface):
         )
 
         print("\t Getting Prompt Template\t")
-        prompt_template = prompt_template_interactor.get_prompt_template(
+        prompt_template = self.prompt_template_interactor.get_prompt_template(
             prompt_template_text=PROMPT_TEXT,
             input_variables=INPUT_VARIABLES
         )
@@ -93,7 +106,7 @@ class GetRAGOutputPresenter(GetRAGOutputPresenterInterface):
             str(source_docs) + '\x1b[' '0m')
 
         try:
-            is_valid_query = validate_text_interactor.validate_results(
+            is_valid_query = self.validate_text_interactor.validate_results(
                 search_result=retrieved_chunks,
                 input_text=input_query)
             if not is_valid_query:
@@ -101,7 +114,7 @@ class GetRAGOutputPresenter(GetRAGOutputPresenterInterface):
         except IndexError:
             return RELEVANT_QUERY
 
-        qa_retrieval = retriever_interactor.get_retrievalqa(
+        qa_retrieval = self.retriever_interactor.get_retrievalqa(
             retriever=retriever,
             llm=local_llm,
             prompt_template=prompt_template
@@ -111,7 +124,7 @@ class GetRAGOutputPresenter(GetRAGOutputPresenterInterface):
         bot_result = bot_response['result']
 
         bot_response_processed = (
-            text_preprocessing_interactor.text_post_processing(
+            self.text_processing_interactor.text_post_processing(
                 text=bot_result,
                 source_docs=source_docs))
 
